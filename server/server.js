a0f7c7ccedb411eda66b58586255445809cd0463
Cesar Martinez <cesaguma@hotmail.com>
2021-03-08 22:16:47 -0500

    Adding imageRoutes variable into server.js file to use /api/imagesRoutes for image upload

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const fileRoutes = require('./routes/file-upload');
 const userRoutes = require('./routes/user-routes');
+const imageRoutes = require('./routes/image-upload'); // add this file import to the top of the file
+
 
 // express middleware, used to be bodyparser
 app.use(express.json());
@@ -17,6 +19,7 @@ if (process.env.NODE_ENV === 'production') {
 // app.use(require('./routes'));
 app.use('/api/', fileRoutes);
 app.use('/api/', userRoutes);
+app.use('/api/', imageRoutes); // add this route for image upload
 
 // Start the API server
 app.listen(PORT, () =>
